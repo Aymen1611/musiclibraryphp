5c59a98c344c62ac3a01f454008a0699fa1d3568
aymen <35670@ma-web.nl>
2023-10-06 13:11:16 +0100

    test

---
+++ public/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Muziek library</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+    <nav class="navbar">
+        <h1 class="navbar-title">Muziek </h1>
+    </nav>
+    <div class="content">
+        <!-- Zoekformulier -->
+        <form id="search-form">
+            <label for="genre"></label>
+            <select id="genre">
+                <option value="">Alle genres</option>
+                <option value="Pop">Pop</option>
+                <option value="Rock">Rock</option>
+                <!-- Voeg hier meer genres toe -->
+            </select>
+            <input type="text" id="search-input" placeholder=" Titel of Artiest...">
+            <button type="submit">Zoeken</button>
+        </form>
+
+        <!-- Resultaten worden hier weergegeven -->
+        <div id="results">
+            
+            <?php
+            // Inclusief het singles.php-document om gegevens op te halen
+            include '../source/data.php';
+
+            foreach ($musicSingles as $single) {
+                include '../source/views/card.php';
+            }
+            ?>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ source/data.php
@@ -0,0 +1,38 @@
+<?php
+// Gegevens van muziek singles
+$musicSingles = [
+    [
+        "title" => "Lijpe - Mansory ft. Frenna (prod. Trobi & Vanno)",
+        "artist" => "Lijpe",
+        "leeftijd" => "33",
+        "genre" => "rap",
+        "image" => "/img/2aecafb8a60d04326d793a1056ccad65.1000x1000x1.png",
+        "link" => "youtube.com"
+    ],
+    [
+        "title" => "BOEF Spitsessie CCCXX Zonamo Underground",
+        "artist" => "BOEF",
+        "leeftijd" => "26",
+        "genre" => "rap",
+        "image" => "/img/artworks-000118349851-5rgxdm-t500x500.jpg"
+    ],
+    [
+        "title" => "Don Toliver - No Idea [Official Music Video]",
+        "artist" => "Don Toliver",
+        "leeftijd" => "36",
+        "genre" => "rap",
+        "image" => "/img/2a4c4b48762d3237f65df17e3ab35a89.1000x1000x1.png"
+    ],
+    [
+        "title" => "XXXTENTACION - Look At Me! (Audio)",
+        "artist" => "XXXTENTACION",
+        "leeftijd" => "22",
+        "genre" => "rap",
+        "image" => "/img/Look_at_Me!.jpg"
+    ],
+    
+
+    
+    
+    // Voeg hier meer gegevens toe
+];
